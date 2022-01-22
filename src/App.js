fa73c937591776077a36e4771295b58533a87035
vleng-gangkangkung <90861876+vleng-gangkangkung@users.noreply.github.com>
2022-01-22 00:06:12 +0000

    Added Github Repo link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        Coded and designed by
+        <a href="https://vleng.co.uk">&nbsp;Vanessa Leng&nbsp;</a>|
+        <a
+          href="https://github.com/vleng-gangkangkung/react-weather-app-project"
+          title="View on Github"
+        >
+          &nbsp;Open Source Code
+        </a>
+      </footer>
+    </div>
+  );
 }
