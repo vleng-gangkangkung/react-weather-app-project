10fbc939d4a0b93142585f27328fb3c0dee57fee
vleng-gangkangkung <90861876+vleng-gangkangkung@users.noreply.github.com>
2022-01-27 00:03:18 +0000

    connected API
    Enabled city, description, windspeed, icon to be linked to live data

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather />
-        <footer>
-          Coded and designed by
-          <a href="https://vleng.co.uk">&nbsp;Vanessa Leng&nbsp;</a>|
-          <a
-            href="https://github.com/vleng-gangkangkung/react-weather-app-project"
-            title="View on Github"
-          >
-            &nbsp;Open Source Code
-          </a>
-        </footer>
+        <div id="panel" className="lightClass">
+          <Weather />
+        </div>
       </div>
+
+      <small>
+        Coded and designed by
+        <a href="https://vleng.co.uk">&nbsp;Vanessa Leng&nbsp;</a>|
+        <a
+          href="https://github.com/vleng-gangkangkung/react-weather-app-project"
+          title="View on Github"
+        >
+          &nbsp;Open Source Code
+        </a>
+      </small>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,36 @@
 import React from "react";
+import "./App.css";
 
 export default function Weather() {
   return (
-    <div className="Weather Container">
-      <div class="form-row">
-        <div class="form-group">
-          <input
-            type="text"
-            class="form-control form-control-md searchCopy"
-            id="search-bar"
-            style="padding-left: 20px;"
-            placeholder="Search city or use your current location"
-          />
-        </div>
-      </div>
+    <div className="Weather">
+      <div className="col-10">
+        <form>
+          <div className="form-row">
+            <div className="col-md-9">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-md searchCopy pl-4"
+                  placeholder="Search city or use your current location"
+                />
+              </div>
+            </div>
 
-      <div class="col-md-2">
-        <div class="form-group">
-          <input
-            type="submit"
-            value="search"
-            class="searchCopy btn"
-            id="submit-btn"
-          />
-        </div>
+            <div className="col-md-2">
+              <div className="form-group">
+                <input
+                  type="submit"
+                  value="search"
+                  className="searchCopy btn"
+                />
+              </div>
+            </div>
+            {/* <div className="col-1 pl-3 ">
+              <Location />
+            </div> */}
+          </div>
+        </form>
       </div>
     </div>
   );
